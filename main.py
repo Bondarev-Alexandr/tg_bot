7e93d352b5961f5fad2b6cf765335d56d3a25aff
Alexandr_Bondarev <bondarev.mzr@gmail.com>
2022-09-09 09:23:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import telebot
+import random
+from telebot import types
+# Загружаем список интересных фактов про Беларусь
+f = open('facts.txt', 'r', encoding='UTF-8')
+facts = f.read().split('\n')
+f.close()
+# Загружаем список беларусских поговорок
+f = open('thinks.txt', 'r', encoding='UTF-8')
+thinks = f.read().split('\n')
+f.close()
+# Создаем бота (+ токен, полученный от @BotFather)
+bot = telebot.TeleBot('5620921991:AAHw6Yf2dAz3HyIUnGRgCcnov6uqfYfAiYk')
+# Команда start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+        # Добавляем две кнопки
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Факт про Беларусь")
+        item2=types.KeyboardButton("Поговорка")
+        markup.add(item1)
+        markup.add(item2)
+        bot.send_message(m.chat.id, 'Нажми: \nФакт для получения интересного факта\nПоговорка — для получения мудрой цитаты',  reply_markup=markup)
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    # Если юзер прислал 1, выдаем ему случайный факт про Беларусь
+    if message.text.strip() == 'Факт про Беларусь' :
+            answer = random.choice(facts)
+    # Если юзер прислал 2, выдаем умную мысль
+    elif message.text.strip() == 'Поговорка':
+            answer = random.choice(thinks)
+    # Отсылаем юзеру сообщение в его чат
+    bot.send_message(message.chat.id, answer)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
